6b6cf1b81878a0418e9ed0b157622eb4c91a7a6e
huangduan <huangduan@huangduan.top>
2017-05-16 22:12:11 +0800

    分配数据

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -10,6 +10,6 @@ class IndexController extends Controller
     //
     public function index()
     {
-        return view('home/index/index');
+        return view('home/index/index', ['name' => 'huangduan']);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,9 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
                 </div>
-
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
