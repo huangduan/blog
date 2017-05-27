449ac9f0f31d0d6acb5654631c9a0b09df5936b1
huangduan <huangduan@huangduan.top>
2017-05-27 21:08:42 +0800

    Update IndexController.php

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -10,6 +10,9 @@ class IndexController extends Controller
     //
     public function index()
     {
+        // 测试
         return view('home/index/index', ['name' => 'huangduan']);
     }
+    
+    
 }
